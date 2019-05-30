dd019566f39524f19308edd73d7b56c2a92aa92a
victor <413469406@qq.com>
2019-05-31 01:22:10 +0800

    管理员编辑页面图片路径的id写错了

--- responsive/php/edit_form.php
+++ responsive/php/edit_form.php
@@ -3,19 +3,15 @@
     echo "<script>alert('你没有权限访问')</script>";
     echo"<script>window.location.href='login.php'</script>";
   }
-  $iid= isset($_GET['id']) ? $_GET['id'] : $_GET['id'];
-  $i=$_GET['id'];
-  $i= $_SESSION["i"];
-  //$_SESSION["iid"]=$iid;
-  //echo $iid;
+  $iid = $_GET["id"];
   if($_SESSION["login"]==2){
-    $i = $_SESSION["id"];
+    $iid = $_SESSION["id"];
   }
 ?>
 
 <?php 
   include('conn.php');
-  $sql = "select image from students where user_name = '$i';";
+  $sql = "select image from students where user_name = '$iid';";
   //echo $sql;
   $result = $conn->query($sql);
   if ($row = mysqli_fetch_array($result))
@@ -27,7 +23,7 @@
 <fieldset>
   <div class="row">
     <div class="col-md-4 col-md-offset-1">
-      <form class="form-horizontal" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+    <form class="form-horizontal" action="<?php echo $_SERVER["PHP_SELF?id= $iid"];?>" method="post">
         <legend>编辑校友</legend>
       <div class="form-group text-center">
         <div class=".col-xs-4">
@@ -58,12 +54,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     //$iid = $_GET['id'];
     // $iid = isset($_SESSION['i']) ? $_SESSION['i'] : '';
     $mo=$_POST["mobile"];
-    //$id=$_SESSION["i"];
     $bu=$_POST["business"];
     $address=$_POST["address"];
-    $i= $_SESSION["i"];
-    //$iid=$_GET['iid'];
-    //echo $iid;
     //  $imag=$_SESSION["userimage"];
     if(!preg_match('/^((\+\s)?86\s\-\s|((\+\s\-\s)?\s86)?)0?1[34579]\d{9}$/',$mo)){
       echo "<script>alert('手机号码格式不正确');</script>";
@@ -73,24 +65,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         set mobile='$mo',
         business='$bu',
         address='$address'
-        where user_name='$i'";
-        echo $sql;
+        where user_name='$iid'";
     //修改数据库
     if(!(mysqli_query($conn,$sql))){
-        echo "<script>alert('编辑失败');</script>";
+        echo "<script>alert('数据编辑失败');</script>";
     }else{
       if(isset($_SESSION["image"])){
         $image=$_SESSION["image"];
         $sqli="update students 
         set image='$image'
-        where user_name='$i'";
+        where user_name='$iid'";
         mysqli_query($conn,$sqli);
         unset($_SESSION["image"]);
+        unset($_SESSION["id"]);
         }
       echo "<script>alert('编辑成功！');</script>";
       if($_SESSION["login"]==1){
-        echo "<script>window.location.href='edit.php?id=$i';</script>";
-        unset($_SESSION["i"]);
+        echo "<script>window.location.href='edit.php?id=$iid';</script>";
       }
       else{
         echo "<script>window.location.href='edit_stu.php';</script>";
